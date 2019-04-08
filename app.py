2a29c892b2a78066ed656bb5fff7faebf69caa3b
Jorge Martinez <jorgemg0327@gmail.com>
2019-04-08 11:23:31 -0500

    Include items using json in register

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
-from flask import Flask
+from flask import (
+    Flask,
+    abort,
+    render_template,
+    request)
+
 from models import User
+
+from functools import wraps
 
 from mongoengine import connect
 
 app = Flask(__name__)
 
+def authorize(f):
+    @wraps(f)
+    def decorated_function(*args, **kws):
+            if not 'Authorization' in request.headers:
+               abort(401)
+
+            user = 'jorge'
+            password = 'martinez'
+
+            # Check database connection
+            test = User.objects(mail='jorge@gmail.com')
+            return f(user, *args, **kws)            
+    return decorated_function
+
+
 @app.route('/')
 def index():
     return "Hello, World!"
 
-@app.route('/register')
-def register():
-    user = User('jorge', 'martinez')
-    user.save()
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    # Render template.
+    if request.method == 'GET':
+        return render_template('register.html')
+    # Insert user into database.
+    if request.method == 'POST':
+        email = request.values.get('email')
+        pw = request.values.get('password')
+
+        user = User(email, password)
+        user.save()
 
 # @app.route('/get')
 # def get():
 
 if __name__ == '__main__':
-    connect('testdb')
+    connect('todo-list')
     app.run()
